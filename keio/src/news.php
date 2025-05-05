02216b33457405f23f0c702f4d3e5ba14be76d99
井上結介 <inoueketsusuke@inotti.local>
2025-05-05 10:21:02 +0900

    header部分が終わりました

--- keio/src/news.php
+++ keio/src/news.php
@@ -26,35 +26,56 @@
   </head>
   <body>
     <header>
-      <div id="header-left">
-        <div id="header_company-name">
-          <p>株式会社<span>ケイオー</span><p>
+      <div id="headerwrapper">
+        <div id="header-left">
+          <div id="header_company-name">
+            <p>株式会社<span>ケイオー</span><p>
+          </div>
+          <div id="header_manu">
+            <ul>
+              <li class="border"><a>アクリルグッズ一覧</a></li>
+              <li class="border"><a>アクキー</a></li>
+              <li class="border"><a>アクスタ</a></li>
+              <li class="border"><a>アクリル時計</a></li>
+              <li class="border"><a>会社概要</a></li>
+            </ul>
+          </div>
         </div>
-        <div id="header_manu">
-          <ul>
-            <li class="border"><a>アクリルグッズ一覧</a></li> <!--　クリックで画面遷移だからaタグ使うべき？ -->
-            <li class="border"><a>アクキー</a></li>
-            <li class="border"><a>アクスタ</a></li>
-            <li class="border"><a>アクリル時計</a></li>
-            <li class="border"><a>会社概要</a></li>
-          </ul>
-        </div>
-      </div>
-      <div id="header-right">
-        <div id="header_access">
-          <a>
-            <div id="header_access-call">
-              <p class="icon"><span><img src="pic/call_black.png"></span>04-7196-0116</p>
-              <p>08:30~17:30（土日祝日定休）</p>
-            </div>
-          </a>
-          <a>
-            <div id="header_access-mail">
-              <p class="icon"><span><img src="pic/mail_black.png"></span>お問い合わせ</p>
-            </div>
-          </a>
+        <div id="header-right">
+          <div id="header_access">
+            <a>
+              <div id="header_access-call">
+                <p class="icon"><span><img src="pic/call_black.png"></span>04-7196-0116</p>
+                <p>08:30~17:30（土日祝日定休）</p>
+              </div>
+            </a>
+            <a>
+              <div id="header_access-mail">
+                <p class="icon"><span><img src="pic/mail_black.png"></span>お問い合わせ</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </header>
+    <div id="topwrapper">
+      <div id="top-left">
+        <div>
+          <h2>お知らせ</h2>
+          <h3>News</h3>
+          <p>ケイオーからのオリジナルアクリルグッズの新商品や営業日などの情報をお知らせします</p>
+        </div>
+      </div>
+      <div id="top-right">
+        <p><span><img src="pic/sms.svg"></span>オリジナルアクリルグッズを依頼する</p>
+        <div>
+          <p><span>オリジナルアクリルグッズ<span><br>について<span>今すぐ相談する<span></p>
+          <!-- ここにチャットスペースくる --!>
+          <div id="top_access-mail">
+            <p class="icon"><span><img src="pic/mail_black.png"></span>お問い合わせ</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
